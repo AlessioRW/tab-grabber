80c17f8690f06fbf201b56ed513c8aa4d456673c
AlessioRW <104003277+AlessioRW@users.noreply.github.com>
2022-06-15 22:57:27 +0100

    Update tab.py

--- tab.py
+++ tab.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.firefox.webdriver import WebDriver
 
 firefox_options = Options()
 firefox_options.add_argument("--headless")
